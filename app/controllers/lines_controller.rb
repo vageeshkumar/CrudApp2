41ca9678e741cf7163d2d4d4db47f27a1a5913f2
Vageesh Kumar <vageeshkumar@gmail.com>
2012-05-21 15:56:02 -0500

    Add delete capability

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -17,6 +17,21 @@ class LinesController < ApplicationController
     @new_line.name = params[:train_line][:name]
     @new_line.frequency = params[:train_line][:frequency]
     @new_line.save
+    flash[:notice] = "New train line was added"
+    redirect_to train_lines_url        
+  end
+  
+  def update
+    @line = TrainLine.find_by_id(params[:id])
+    @line.name = params[:train_line][:name]
+    @line.frequency = params[:train_line][:frequency]
+    @line.save
+    redirect_to train_lines_url
+  end
+  
+  def destroy
+    line = TrainLine.find_by_id(params[:id])
+    line.destroy
     redirect_to train_lines_url    
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Crud2App::Application.routes.draw do
 get "lines", :controller => "lines", :action => "index", :as => :train_lines
 get "couple", :controller => "lines", :action => "couple", :as => :couple_page
 get "lines/new", :controller => "lines", :action => "new", :as => :new_train_line
+put "lines/edit/:id", :controller => "lines", :action => "update"
 get "lines/edit/:id", :controller => 'lines', :action => 'edit', :as =>:edit_train_line
 post "lines", :controller => "lines", :action => "create" 
-
+delete "lines/:id" => 'lines#destroy'
 get "lines/:id", :controller => "lines", :action => "individual", :as => :individual_train
 
 end
